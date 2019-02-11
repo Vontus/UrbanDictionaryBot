3b5bd2a2bb5c54f34ed63653db7c5b9704f316bd
Alberto Martínez <amartinez@lynxview.es>
2019-02-12 00:55:17 +0100

    Add info as logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,9 +2,11 @@ import Debug from 'debug';
 let log = Debug('tgbot')
 let debug = log.extend('debug')
 let error = log.extend('error')
+let info = log.extend('info')
 
 export default {
   log,
   debug,
-  error
+  error,
+  info
 }
