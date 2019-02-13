d2d1ff094d13772bc4a8e095d250d3633b918ec0
Alberto Martínez <amartinez@lynxview.es>
2019-02-13 23:13:16 +0100

    More log levels

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,14 @@
 import Debug from 'debug';
-let log = Debug('tgbot')
+let log = Debug('udbot')
 let debug = log.extend('debug')
+let warn = log.extend('warn')
 let error = log.extend('error')
 let info = log.extend('info')
 
 export default {
   log,
   debug,
+  warn,
   error,
   info
 }
