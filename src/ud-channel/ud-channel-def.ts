7b5f30564d60a743ddb92e26e813142d70638e32
Alberto Martínez <amartinez@lynxview.es>
2019-03-17 23:50:00 +0100

    Add sentDate to channel definitions

--- src/ud-channel/ud-channel-def.ts
+++ src/ud-channel/ud-channel-def.ts
@@ -1,9 +1,11 @@
 export class UdChannelDef {
   defId: number
   gif: string
+  sentDate: Date
 
-  public constructor (defId: number, gif: string) {
+  public constructor (defId: number, gif: string, sentDate: Date) {
     this.defId = defId
     this.gif = gif
+    this.sentDate = sentDate
   }
 }
