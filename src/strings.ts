814e2639ead7b43a4972de5a6175ab8cf9c16fd8
Alberto <albertomk8@gmail.com>
2020-05-30 14:34:04 +0200

    Change donation link

--- src/strings.ts
+++ src/strings.ts
@@ -2,11 +2,12 @@ export default {
   help: 'I can help you find out about english slang by sending a message here or via inline mode! 😎',
   noResults: "¯\\_(ツ)_/¯\nThere aren't any definitions for <b>{0}</b> yet.",
   arabicResponse: 'This bot is only available in English.\nاین ربات فقط به زبان انگلیسی کار می‌کند.',
+  donateLink: 'https://www.buymeacoffee.com/vontus',
 
   commands: {
     start: 'Type the word or expression you want to search.',
     about: 'Bot created by @Vontus using this awesome <a href="https://github.com/yagop/node-telegram-bot-api">API</a>.\n\n<a href="https://github.com/Vontus/UrbanDictionaryBot">Bot source</a>',
-    donate: 'Building bots like this consumes time and money. If you like this bot and want to help me keep the server up, you can donate by clicking <a href="http://paypal.me/vontus">here</a>. Thank you! ❤️',
+    donate: 'Help me stay awake and coding! <a href="https://www.buymeacoffee.com/vontus">Buy me a coffee ☕️</a>.',
     stats: {
       dateFormat: 'YYYY-MM-DD',
       wrongDateFormat: 'Date {0} is not valid. Please use format {1}.',
